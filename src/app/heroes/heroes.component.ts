5c7af46e5e2f53521cee40b78f99d1f05520259b
Josu Prados <pradosotx@gmail.com>
2022-01-12 20:59:16 +0100

    feature component

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
 import { HEROES } from './mock-heroes';
 
+
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
@@ -12,10 +13,7 @@ export class HeroesComponent {
   heroes = HEROES;
 
   selectedHero?: Hero;
-  hero: Hero = {
-    id: 1,
-    name: 'Windstorm'
-  }
+
 
   onSelect(hero:Hero): void {
     this.selectedHero = hero;
