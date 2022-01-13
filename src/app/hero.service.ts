e109d9ff1aa06f4e9c22e1a29cf92782dc3d9c14
Josu Prados <pradosotx@gmail.com>
2022-01-13 20:00:45 +0100

    Hero sevice

---
+++ src/app/hero.service.ts
@@ -0,0 +1,13 @@
+import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import { Hero } from './hero';
+import { HEROES } from './heroes/mock-heroes';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HeroService {
+  getHeroes(): Observable<Hero[]> {
+    return of(HEROES);
+  }
+}
--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
-import { HEROES } from './mock-heroes';
+import { HeroService } from '../hero.service';
 
 
 @Component({
@@ -8,14 +8,24 @@ import { HEROES } from './mock-heroes';
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css']
 })
-export class HeroesComponent {
+export class HeroesComponent implements OnInit {
 
-  heroes = HEROES;
+  heroes : Hero[]= [];
 
   selectedHero?: Hero;
 
 
-  onSelect(hero:Hero): void {
+  constructor(private heroService : HeroService){}
+
+  ngOnInit(): void {
+    this.getHeroes();
+
+  }
+
+  getHeroes(){
+    this.heroService.getHeroes().subscribe(heroesRecibidos=>this.heroes = heroesRecibidos)
+  }
+ onSelect(hero:Hero): void {
     this.selectedHero = hero;
   }
 }
